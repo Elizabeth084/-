a305e2e634398d46b5128d0c9729bda4675bdb6d
Elizabeth <yelizaveta.bashkirova@bk.ru>
2021-10-01 20:19:16 +0300

    Practise

---
+++ practise_2/3.py
@@ -0,0 +1,9 @@
+def tss()
+    print("Введите входные данные(1 число)")
+    time = int(input("Количество минут - "))
+    print("Ответ:")
+    hours = time // 60
+    minutes = time % 60
+    return "time -"+ str(hours % 24) + ":" + str(minutes)
+    print(tss())
+   
